c1952c176521d7e7141ce3b122887cf4106d8939
Afonso Matos <afonsolfmatos@gmail.com>
2020-01-11 15:28:05 +0000

    Divide webpack into dev vs production.

---
+++ webpack.prod.js
@@ -0,0 +1,48 @@
+const webpack = require("webpack");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require("path")
+
+module.exports = {
+    entry: ["./src/index.ts"],
+    watch: false,
+    mode: "none",
+    performance: { hints: false },
+    module: {
+      rules: [
+        {
+            test: /\.scss$/,
+            use: [
+              'vue-style-loader',
+              'css-loader?url=false'
+            ]
+        },
+        {
+          test: /\.scss$/,
+          use: 'sass-loader'
+        },
+        {
+            test: /\.vue$/,
+            loader: "vue-loader"
+        },
+        {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: "ts-loader",
+            options: {
+                appendTsSuffixTo: [/\.vue$/],
+            }
+        },
+      ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue'],
+        alias: {
+            '@': path.resolve(__dirname, 'src/'),
+            vue$: 'vue/dist/vue.esm.js'
+        },
+    },
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new VueLoaderPlugin()
+    ]
+  };
